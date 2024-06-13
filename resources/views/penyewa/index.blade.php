c9a34027a562e3a0c23c8905cca2ee2ff0f1f319
yusac3 <mybogang@gmail.com>
2024-06-14 02:10:44 +0700

    menambahkan isi index pada penyewa

--- resources/views/penyewa/index.blade.php
+++ resources/views/penyewa/index.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $pageTitle }}</title>
+    <title> </title>
     @vite('resources/sass/app.scss')
 </head>
 <body>
+
     <nav class="navbar navbar-expand-md navbar-dark bg-primary">
         <div class="container">
             <a href="{{ url('/homee') }}" class="navbar-brand mb-0 h1"><i class="bi-hexagon-fill me-2"></i> Penyewa Dashboard</a>
@@ -15,15 +16,50 @@
             </form>
             <a href="{{ route('logout') }}" class="btn btn-outline-light my-2 ms-md-2"
                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout
-            </a>    
+            </a>
     </nav>
     <div class="container mt-4">
         <h4>Penyewa Dashboard</h4>
-        <ul>
-            <li><a href="">Search Kos-Kosan</a></li>
+        <table class="table table-bordered table-hover table-striped mb-0 bg-white" id="tableKos">
+            <thead>
+                <tr>
+                    <th>Nama</th>
+                    <th>Alamat</th>
+                    <th>Deskripsi</th>
+                    <th>Harga</th>
+                    <th>Ketersediaan</th>
+                    <th>Nama Pemilik</th>
+                    <th>Gambar</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($kosts as $kost)
+                    @foreach ($kost->rooms as $room)
+                        <tr>
+                            <td>{{ $kost->name }}</td>
+                            <td>{{ $kost->address }}</td>
+                            <td>{{ $kost->description }}</td>
+                            <td>{{ $room->price }}</td>
+                            <td>{{ $room->availability ? 'Tersedia' : 'Tidak Tersedia' }}</td>
+                            <td>{{ $room->description }}</td>
+                            <td>
+                                @if ($kost->encrypted_photoname)
+                                    <img src="{{ asset('storage/files/DocumentPhotoKost/' . $kost->encrypted_photoname) }}"
+                                        alt="{{ $kost->name }}" style="width: 100px; height: auto;">
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                @endforeach
+            </tbody>
+        </table>
 
 
-        </ul>
+
+
+
+
     </div>
     @vite('resources/js/app.js')
 </body>
